502312c3b55703471a5cc55468557e13c21db1f3
Anjali Garg <garga2709@gmail.com>
2025-01-09 22:11:46 +0530

    Throw exception for negative numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -7,8 +7,14 @@ class StringCalculator {
       delimiter = new RegExp(`[${delimiterPart[2]}]`);
       numbers = numbers.split("\n")[1];
     }
-    const numArray = numbers.split(delimiter);
-    return numArray.reduce((sum, num) => sum + parseInt(num), 0);
+
+    const numArray = numbers.split(delimiter).map(Number);
+    const negatives = numArray.filter((n) => n < 0);
+    if (negatives.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negatives.join(",")}`);
+    }
+    return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
+
 module.exports = StringCalculator;
