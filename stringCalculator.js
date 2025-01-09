dfc7dcfef55813c73446d9c1d9c74ff587850479
Anjali Garg <garga2709@gmail.com>
2025-01-09 22:01:15 +0530

    Support custom single character delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,13 @@
 class StringCalculator {
   add(numbers) {
     if (numbers === "") return 0;
-    const numArray = numbers.split(/[\n,]/);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const delimiterPart = numbers.split("\n")[0];
+      delimiter = new RegExp(`[${delimiterPart[2]}]`);
+      numbers = numbers.split("\n")[1];
+    }
+    const numArray = numbers.split(delimiter);
     return numArray.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
