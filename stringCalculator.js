7de8b5468d665201263d72687b31a951fe1be755
Anjali Garg <garga2709@gmail.com>
2025-01-09 21:43:37 +0530

    Handle single number input

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,7 @@
 class StringCalculator {
   add(numbers) {
     if (numbers === "") return 0;
+    return parseInt(numbers);
   }
 }
 module.exports = StringCalculator;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,3 +5,9 @@ test("should return 0 for an empty string", () => {
   const calculator = new StringCalculator();
   expect(calculator.add("")).toBe(0);
 });
+
+// Adding support for a single number.
+test("should return the number when only one number is provided", () => {
+  const calculator = new StringCalculator();
+  expect(calculator.add("1")).toBe(1);
+});
